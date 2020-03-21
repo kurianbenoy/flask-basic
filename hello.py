43f9fa2a0c3013edb0f160d57eff66b9bb98f154
kurianbenoy <kurian.bkk@gmail.com>
2020-03-21 20:01:42 +0530

    add COVID-19 get request

--- hello.py
+++ hello.py
@@ -1,6 +1,13 @@
 from flask import Flask
+
+import requests
 app = Flask(__name__)
 
 @app.route('/hello/')
 def hello_world():
     return ('Hello, World!')
+
+@app.route('/india_covid/', methods=['GET','POST'])
+def no_case():
+    r = requests.get('https://covid19indiaapi.herokuapp.com/v1/overall')
+    return r.json()
